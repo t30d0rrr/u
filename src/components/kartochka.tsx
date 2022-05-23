1a075e4bb53bca342b9ec602aefcba19ac28c3af
t30d0rrr <make.it.supercool@gmail.com>
2022-05-24 00:20:55 +0300

    ass pain 2

---
+++ src/components/kartochka.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function карточка(){
+  return (
+    <div className="карточка">
+      <header className="карточка-header">
+      карточка пук
+      </header>
+    </div>
+  );
+}
